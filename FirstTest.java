eaa122132ce5559532e4337fbdbb435ae3b74cca
Anton-Miheev <miheev_anton@mail.ru>
2022-05-12 18:13:10 +0300

    Initial commit

---
+++ FirstTest.java
@@ -0,0 +1,36 @@
+package test1.java.ru.gb.test;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+public class FirstTest {
+    @Test
+    public void checkEmptyEmail() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel 5 API 30");
+        capabilities.setCapability("platformVersion", "11");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", "/Install/Android-NativeDemoApp-0.2.1.apk");
+        MobileDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+
+        Thread.sleep(2000);
+        MobileElement loginMenuButton = (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"Login\"]/android.widget.TextView");
+        loginMenuButton.click();
+        Thread.sleep(2000);
+        MobileElement loginButton = (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"button-LOGIN\"]/android.view.ViewGroup");
+        loginButton.click();
+        Thread.sleep(2000);
+        MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
+        Assert.assertEquals(errorText.getText(), "Please enter a valid email address");
+    }
+
+
+}
